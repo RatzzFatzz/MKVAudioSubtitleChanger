cf2fdcb30e27f4404f1c0fbfb844402a9465c529
RatzzFatzz <michael@ratzloeffel.de>
2020-04-05 19:30:52 +0200

    [IMPL] Default Kernel

--- src/main/java/at/pcgamingfreaks/mkvaudiosubtitlechanger/AttributeUpdaterKernel.java
+++ src/main/java/at/pcgamingfreaks/mkvaudiosubtitlechanger/AttributeUpdaterKernel.java
@@ -1,12 +1,30 @@
 package at.pcgamingfreaks.mkvaudiosubtitlechanger;
 
 import at.pcgamingfreaks.mkvaudiosubtitlechanger.config.AttributeConfig;
+import at.pcgamingfreaks.mkvaudiosubtitlechanger.intimpl.MkvFileCollector;
+import at.pcgamingfreaks.mkvaudiosubtitlechanger.model.FileAttribute;
 import at.pcgamingfreaks.mkvaudiosubtitlechanger.util.ConfigUtil;
 
+import java.io.File;
 import java.util.List;
+import java.util.Scanner;
 
 public class AttributeUpdaterKernel {
+    MkvFileCollector collector = new MkvFileCollector();
     public void execute() {
-        List<AttributeConfig> list = ConfigUtil.loadConfig();
+        List<AttributeConfig> configPattern = ConfigUtil.loadConfig();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the path to the files which should be updated: ");
+        List<File> allValidPaths = collector.loadFiles(scanner.nextLine());
+
+        for(File file: allValidPaths) {
+            List<FileAttribute> attributes = collector.loadAttributes(file);
+            for(AttributeConfig config: configPattern) {
+                boolean fileIsChanged = config.processConfig(file, attributes);
+                if(fileIsChanged) {
+                    break;
+                }
+            }
+        }
     }
 }
--- src/main/java/at/pcgamingfreaks/mkvaudiosubtitlechanger/config/AttributeConfig.java
+++ src/main/java/at/pcgamingfreaks/mkvaudiosubtitlechanger/config/AttributeConfig.java
@@ -1,7 +1,9 @@
 package at.pcgamingfreaks.mkvaudiosubtitlechanger.config;
 
+import at.pcgamingfreaks.mkvaudiosubtitlechanger.model.FileAttribute;
 import lombok.Getter;
 
+import java.io.File;
 import java.util.List;
 
 @Getter
@@ -12,5 +14,18 @@ public class AttributeConfig {
     public AttributeConfig(List<String> audio, List<String> subtitle) {
         this.audio = audio;
         this.subtitle = subtitle;
+    }
+
+    /**
+     * Processes the config lists and apply the changes if the combination matches
+     * @return If the current configuration matched and changes applied or not
+     */
+    public boolean processConfig(File file, List<FileAttribute> attributes) {
+        // check if size is bigger or equal 2 to make sure that there is at least one audio and subtitle line
+        // TODO: implement empty audio or subtitle line
+        if(attributes.size() >= 2) {
+           // TODO: Update queryBuilder:updateAttributes here
+        }
+        return true;
     }
 }
